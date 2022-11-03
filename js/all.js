3ba00267772dc386560867c83fd7337b23b0aa02
wboirnk <wboirnk@gmail.com>
2022-11-03 14:21:19 +0800

    bug fix

--- js/all.js
+++ js/all.js
@@ -1,18 +1,21 @@
 const nav = document.querySelector("nav");
 const logoBig = document.querySelector(".logoBig");
 const slogan=document.querySelector(".slogan")
+const menuBtn=document.querySelector(".menu-fix-btn")
 function navOn() {
   if (window.scrollY > 0) {
     nav.classList.add("navOn");
-    logoBig.classList.add("logoOut");
-    slogan.classList.add("sloganOut")
+    // logoBig.classList.add("logoOut");
+    // slogan.classList.add("sloganOut");
+    menuBtn.classList.add("menu-on");
   } else {
     nav.classList.remove("navOn");
-    logoBig.classList.remove("logoOut");
-    slogan.classList.remove("sloganOut")
+    // logoBig.classList.remove("logoOut");
+    // slogan.classList.remove("sloganOut")
+    menuBtn.classList.remove("menu-on");
   }
 }
 
 window.addEventListener("scroll", navOn);
 
-console.log(nav);
+// console.log(nav);
