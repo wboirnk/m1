a6ee68fd1d8aba10c773d0b776c849a69f09f25f
wboirnk <wboirnk@gmail.com>
2022-11-08 12:37:46 +0800

    winter link done

--- js/nav.js
+++ js/nav.js
@@ -13,18 +13,13 @@ function navOn() {
 window.addEventListener("scroll", navOn);
 
 const footerPc = document.querySelector(".footer-pc");
-const footerMb= document.querySelector(".footer-mb")
+const footerMb = document.querySelector(".footer-mb");
 window.onscroll = () => {
   let cHeight = document.documentElement.clientHeight;
   let sHeight = document.documentElement.scrollHeight;
   let sTop = document.documentElement.scrollTop;
-  console.log("b");
-  
-  if (sTop>5328||sHeight == cHeight + sTop) {
-    console.log("a");
+  if (sTop > 5328 || sHeight == cHeight + Math.floor(sTop)) {
     footerPc.classList.add("footer-on");
     footerMb.classList.add("footerMb-on");
   }
-
-//   }
 };
