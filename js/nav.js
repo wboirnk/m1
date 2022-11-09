2146bc80604b83dc3bbc6a1ff1f04ad3656fffd3
wboirnk <wboirnk@gmail.com>
2022-11-09 10:09:29 +0800

    mac js test

--- js/nav.js
+++ js/nav.js
@@ -18,7 +18,13 @@ window.onscroll = () => {
   let cHeight = document.documentElement.clientHeight;
   let sHeight = document.documentElement.scrollHeight;
   let sTop = document.documentElement.scrollTop;
+  console.log("scroll");
   if (sTop > 5328 || sHeight == cHeight + Math.floor(sTop)) {
+    if (sTop>5328) {
+      console.log("5328");
+    }else if (sHeight == cHeight + Math.floor(sTop)){
+      console.log("down");
+    }
     footerPc.classList.add("footer-on");
     footerMb.classList.add("footerMb-on");
   }
