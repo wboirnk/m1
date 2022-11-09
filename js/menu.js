c0741ff63a3be52f7f3f7f2a687929d5cfeb5b24
wboirnk <wboirnk@gmail.com>
2022-11-09 14:12:44 +0800

    1109

--- js/menu.js
+++ js/menu.js
@@ -254,3 +254,45 @@ milkTea.addEventListener('click',function(){
 
 
 
+// window.onload.setTimeout(()=>{
+//   const body = document.querySelector("body");
+//   const bodyMask = document.querySelector(".loading");
+//   body.classList.add("loading-all");
+//   bodyMask.classList.add("loading-done");
+// },3000)
+
+window.onload=()=>{
+    setTimeout(function(){
+      const body = document.querySelector("body");
+    const bodyMask = document.querySelector(".loading");
+    body.classList.add("loading-all");
+    bodyMask.classList.add("loading-done");
+    },1500)
+  }
+  
+  // window.onload = () => {
+  //   const body = document.querySelector("body");
+  //   const bodyMask = document.querySelector(".loading");
+  //   body.classList.add("loading-all");
+  //   bodyMask.classList.add("loading-done");
+  // };
+  
+  const nav = document.querySelector("nav");
+  const menuBtn = document.querySelector(".menu-fix-btn");
+
+  
+  const footerPc = document.querySelector(".footer-pc");
+  const footerMb = document.querySelector(".footer-mb");
+  window.onscroll = () => {
+    let cHeight = document.documentElement.clientHeight;
+    let sHeight = document.documentElement.scrollHeight;
+    let sTop = document.documentElement.scrollTop;
+    if (sTop > 5328 || sHeight == cHeight + Math.floor(sTop)) {
+      if (sTop>5328) {
+      }else if (sHeight == cHeight + Math.floor(sTop)){
+      }
+      footerPc.classList.add("footer-on");
+      footerMb.classList.add("footerMb-on");
+    }
+  };
+  
