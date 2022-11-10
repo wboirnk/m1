16333846037570dc3b2563c62fa860609fe2e575
binbro708 <b86070800@gmail.com>
2022-11-10 21:20:49 +0800

    1110

--- js/nav.js
+++ js/nav.js
@@ -41,7 +41,7 @@ const footerAnimation = gsap.timeline({
   scrollTrigger: {
     trigger: "footer", // 決定scrolltrigger要以哪一個元素作為觸發基準點
     markers: false, // 開啟start & end標記點，單純方便瀏覽動畫開始與結束點
-    start: "top 80%", // 決定動畫開始點的位置
+    start: "top 87%", // 決定動畫開始點的位置
     end: "top 1%", // 決定動畫結束點的位置
     // scrub: true, //重要！開啟scrub來決定動畫播放是否依賴視窗滾動
   },
